e2183dd99769e2ea8d8ae232b5c19a9a8b426d90
Londre Blocker <londre.blocker@gmail.com>
2016-04-24 19:10:59 -0400

    Par 4 completed

---
+++ Interview/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Interview
+//
+//  Created by Londre Blocker on 4/24/16.
+//  Copyright © 2016 Londre Blocker. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
